5e66debefa60d1eec8d829c1d8b5d6bda9be7379
Roman Rozhalin <rozromario@gmail.com>
2022-08-19 21:17:52 +0300

    -Part2

--- src/main/java/ConvertAmount.java
+++ src/main/java/ConvertAmount.java
@@ -2,22 +2,41 @@ import java.util.Scanner;
 
 public class ConvertAmount {
     public static void main(String[] args) {
-        final double ROUBLES_PER_EURO = 67.68;
+        final double ROUBLES_PER_DOLLAR = 72.12;
 
-        int euros;
+        int dollars;
         double roubles;
+        int digit;
 
         Scanner input = new Scanner(System.in);
 
-        //Получить сумму денег в евро
-        System.out.print("Введите сумму денег в евро: ");
-        euros = input.nextInt();
+        //Получить сумму денег в долларах
+        System.out.print("Введите сумму денег в американских долларах: ");
+        dollars = input.nextInt();
+
+        //Отобразить сумму денег в американских долларах
+        //с правильным окончанием
+        System.out.print(dollars);
+
+        if (5 <= dollars && dollars <= 20) {
+            System.out.print(" американских долларов равны ");
+        } else {
+            digit = dollars % 10;
+
+            if (digit == 1) {
+                System.out.print(" американский доллар равен ");
+            } else if (2 <= digit && digit <= 4) {
+                System.out.print(" американских доллара равны ");
+            } else {
+                System.out.print(" американских долларов равны ");
+            }
+        }
 
         //Конвертировать сумму денег в российские рубли
-        roubles = ROUBLES_PER_EURO * euros;
+        roubles = ROUBLES_PER_DOLLAR * dollars;
 
         //Отобразить сумму денег в российских рублях в пользу покупателя
-        System.out.println("Она равна " + (int)(roubles * 100) / 100.0
+        System.out.println((int)(roubles * 100) / 100.0
                 + " российского рубля.");
     }
 }
