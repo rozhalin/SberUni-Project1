8ecb54f39747638fcf5858d3da6a42e5467d0b08
Roman Rozhalin <rozromario@gmail.com>
2022-08-29 14:03:10 +0300

    -Part3

--- src/main/java/ConvertAmount.java
+++ src/main/java/ConvertAmount.java
@@ -7,36 +7,52 @@ public class ConvertAmount {
         int dollars;
         double roubles;
         int digit;
+        int n;
+        int i;
 
         Scanner input = new Scanner(System.in);
 
-        //Получить сумму денег в долларах
-        System.out.print("Введите сумму денег в американских долларах: ");
-        dollars = input.nextInt();
+        //Получать количество конвертаций по тех пор,
+        //пока не введено корректное значение
+        do {
+            System.out.print("Введите корректное количество конвертаций: ");
+            n = input.nextInt();
+        } while (n <= 0);
 
-        //Отобразить сумму денег в американских долларах
-        //с правильным окончанием
-        System.out.print(dollars);
+        //До тех пор, пока не конвертированы все суммы, получать,
+        //отображать и конвертировать суммы денег в американских
+        //долларах и отображать суммы денег в российских рублях
 
-        if (5 <= dollars && dollars <= 20) {
-            System.out.print(" американских долларов равны ");
-        } else {
-            digit = dollars % 10;
+        for (i = 0; i < n; ++i) {
 
-            if (digit == 1) {
-                System.out.print(" американский доллар равен ");
-            } else if (2 <= digit && digit <= 4) {
-                System.out.print(" американских доллара равны ");
-            } else {
+            //Получить сумму денег в долларах
+            System.out.print("Введите сумму денег в американских долларах: ");
+            dollars = input.nextInt();
+
+            //Отобразить сумму денег в американских долларах
+            //с правильным окончанием
+            System.out.print(dollars);
+
+            if (5 <= dollars && dollars <= 20) {
                 System.out.print(" американских долларов равны ");
+            } else {
+                digit = dollars % 10;
+
+                if (digit == 1) {
+                    System.out.print(" американский доллар равен ");
+                } else if (2 <= digit && digit <= 4) {
+                    System.out.print(" американских доллара равны ");
+                } else {
+                    System.out.print(" американских долларов равны ");
+                }
             }
+
+            //Конвертировать сумму денег в российские рубли
+            roubles = ROUBLES_PER_DOLLAR * dollars;
+
+            //Отобразить сумму денег в российских рублях в пользу покупателя
+            System.out.println((int) (roubles * 100) / 100.0
+                    + " российского рубля.");
         }
-
-        //Конвертировать сумму денег в российские рубли
-        roubles = ROUBLES_PER_DOLLAR * dollars;
-
-        //Отобразить сумму денег в российских рублях в пользу покупателя
-        System.out.println((int)(roubles * 100) / 100.0
-                + " российского рубля.");
     }
 }
